16609f1674bafc102b251e112d983e6111980730
Francisndungu2 <francianmbugua@gmail.com>
2020-11-08 18:36:00 +0300

    add componentquote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateQuoteComponent } from './create-quote/create-quote.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateQuoteComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
