a7d0f280b81591617e08d702c99a80a90e32b678
Francisndungu2 <francianmbugua@gmail.com>
2020-11-08 21:15:28 +0300

    modify

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('QUOTE app is running!');
+    expect(page.getTitleText()).toEqual('mQuote app is running!');
   });
 
   afterEach(async () => {
